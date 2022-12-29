c4c2c6691b78ebc1be056550472f44861f4b3862
seohee0112 <ghdtj7@naver.com>
2022-12-29 23:35:37 +0900

    chore : eslint 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
