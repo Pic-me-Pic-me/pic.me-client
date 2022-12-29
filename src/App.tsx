2c347d537064980b04d386d7aa60325f99a4b087
seohee0112 <ghdtj7@naver.com>
2022-12-29 20:52:43 +0900

    init : yarn berry 설정

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
