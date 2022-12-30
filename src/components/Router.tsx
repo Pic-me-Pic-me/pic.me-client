52750e47a10a695d6a9b8e05b6b25f06ea06de35
seohee0112 <ghdtj7@naver.com>
2022-12-30 21:07:38 +0900

    feat : 라우터 구현

---
+++ src/components/Router.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+
+import Error404 from '../pages/Error404';
+import Home from '../pages/Home';
+import Library from '../pages/Library';
+import Login from '../pages/Login';
+import MakerLanding from '../pages/MakerLanding';
+import PlayerLanding from '../pages/PlayerLanding';
+import Signup from '../pages/Signup';
+import Voting from '../pages/Voting';
+
+const Router = () => (
+  <BrowserRouter>
+    <RecoilRoot>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/playerlanding" element={<PlayerLanding />} />
+        <Route path="/makerlanding" element={<MakerLanding />} />
+        <Route path="/voting" element={<Voting />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </RecoilRoot>
+  </BrowserRouter>
+);
+
+export default Router;
