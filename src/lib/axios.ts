d89009e881490d2ae319e18490d04d20ad38cc70
seohee0112 <ghdtj7@naver.com>
2023-01-01 10:31:48 +0900

    docs : client로 네이밍 변경

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseInstance = axios.create({
+export const client = axios.create({
   //baseURL: `${process.env.REACT_APP_IP}`,
   headers: {
     'Content-type': 'application/json',
