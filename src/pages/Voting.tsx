70b4cae4797aff16e962b5d90b3bb5c8ce188866
seohee0112 <ghdtj7@naver.com>
2023-01-01 10:34:14 +0900

    feat : 투표 정보 가져와서 보여주기 구현

--- src/pages/Voting.tsx
+++ src/pages/Voting.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 
-const Voting = () => <div>투표</div>;
+import { Error, Loading } from '../components/common';
+import { Header } from '../components/Voting';
+import { useGetVotingInfo } from '../lib/hooks/voting';
+import { votingStateSelector } from '../recoil/player/selector';
 
+const Voting = () => {
+  const { votingInfo, isLoading, isError } = useGetVotingInfo(1);
+  const [newVotingInfo, setNewVotingInfo] = useRecoilState(votingStateSelector);
+  useEffect(() => {
+    if (votingInfo) {
+      setNewVotingInfo(votingInfo.data);
+    }
+  }, []);
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
+
+  return (
+    <div>
+      <p>{newVotingInfo.vote_title}</p>
+      <Header />
+    </div>
+  );
+};
 export default Voting;
