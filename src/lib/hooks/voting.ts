c0c5e2ca936c5fd70c3789c33cdc0860f6799930
seohee0112 <ghdtj7@naver.com>
2023-01-01 10:32:12 +0900

    feat : 투표 가져오기 커스텀 훅 구현

--- src/lib/hooks/voting.ts
+++ src/lib/hooks/voting.ts
@@ -1,11 +1,11 @@
 import { AxiosResponse } from 'axios';
 import useSWR from 'swr';
 
-import { baseInstance } from '../axios';
+import { client } from '../axios';
 import { VotingInfo } from './../../types/voting';
 
 export const useGetVotingInfo = (vote_id: number) => {
-  const { data, error } = useSWR<AxiosResponse<VotingInfo>>(`/vote?vote_id=${vote_id}`, baseInstance.get, {
+  const { data, error } = useSWR<AxiosResponse<VotingInfo>>(`/vote?vote_id=${vote_id}`, client.get, {
     // 실패시 재요청 3번
     errorRetryCount: 3,
   });
