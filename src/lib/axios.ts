2ab1d24b2ca716c105fb2fa90bb9effd47ff4c0a
seohee0112 <ghdtj7@naver.com>
2023-01-01 10:20:07 +0900

    feat : axios 인스턴스 생성

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const reqAPI = axios.create({
-  baseURL: `${process.env.REACT_APP_IP}`,
+export const baseInstance = axios.create({
+  //baseURL: `${process.env.REACT_APP_IP}`,
   headers: {
     'Content-type': 'application/json',
   },
