b09e67c3d85783a3606d857e1cd662ec9891f4e6
Dev-jwJeong <rgn95652@gmail.com>
2023-01-03 23:20:37 +0900

    feat : 이미지 새로고침 유지를 위한 상태관리

--- src/recoil/maker/atom.ts
+++ src/recoil/maker/atom.ts
@@ -1,5 +1,17 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
+interface VotingImage {
+  firstImageUrl: string;
+  secondImageUrl: string;
+}
 //페이지가 변경되더라도 상태관리를 유지
 const { persistAtom } = recoilPersist();
+export const votingImageState = atom<VotingImage>({
+  key: 'votingImage',
+  default: {
+    firstImageUrl: '',
+    secondImageUrl: '',
+  },
+  effects_UNSTABLE: [persistAtom],
+});
