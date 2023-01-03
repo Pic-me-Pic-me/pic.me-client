f02e08ac135a95edc849c08cdfff240e71a15d46
Dev-jwJeong <rgn95652@gmail.com>
2023-01-03 17:34:40 +0900

    style : 메이커 투표생성 페이지

---
+++ src/pages/MakerVoting.tsx
@@ -0,0 +1,10 @@
+import { ImageInput, TitleInput } from '../components/MakerVoting';
+
+const MakerVoting = () => (
+  <div>
+    <TitleInput />
+    <ImageInput />
+  </div>
+);
+
+export default MakerVoting;
