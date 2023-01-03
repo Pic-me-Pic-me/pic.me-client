987dabde1616ef9e961d3dbc77fd1a013fa55155
Dev-jwJeong <rgn95652@gmail.com>
2023-01-03 22:39:25 +0900

    style : 이미지 파일 명 변경

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,1 +1,2 @@
+export { ReactComponent as IcImageAdd } from './ic_image_add.svg';
 export { ReactComponent as IcPickmeSticker } from './picme_sticker.svg';
--- src/asset/image/index.ts
+++ src/asset/image/index.ts
@@ -1,2 +1,1 @@
 export { default as FaceSticker } from './face_sticker.png';
-export { ReactComponent as ImageAdd } from './image_add.svg';
