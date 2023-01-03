16e1c5acdc5ec1e7e83f67c5c11bbe89cb2f968b
parkhyeonji <iamphj3@naver.com>
2023-01-04 01:42:12 +0900

    style : 홈 화면(현재 진행중인 투표)  UI 구현

--- src/components/Home/VoteList.tsx
+++ src/components/Home/VoteList.tsx
@@ -4,11 +4,31 @@ import styled from 'styled-components';
 import { EmptyIcon } from '../../asset/image';
 
 const VoteList = () => {
-  const [voteList, setVoteList] = useState<string[]>();
+  const [voteList, setVoteList] = useState<string[]>(['투표']);
   return (
     <>
       {voteList ? (
-        <></>
+        <StVoteListWrapper>
+          <h1>현재 진행중인 투표</h1>
+          <StVoteList>
+            <StVoteItem>
+              <StVoteData>
+                <h1>어제 연남동 가서 찍은...</h1>
+                <h2>15명 투표 중</h2>
+                <h3>43분 전</h3>
+              </StVoteData>
+              <StPreviewImg />
+            </StVoteItem>
+            <StVoteItem>
+              <StVoteData>
+                <h1>어제 연남동 가서 찍은...</h1>
+                <h2>15명 투표 중</h2>
+                <h3>43분 전</h3>
+              </StVoteData>
+              <StPreviewImg />
+            </StVoteItem>
+          </StVoteList>
+        </StVoteListWrapper>
       ) : (
         <StEmptyView>
           <img src={EmptyIcon} alt="현재 진행중인 투표 없음" />
@@ -22,7 +42,69 @@ const VoteList = () => {
 
 export default VoteList;
 
-const StEmptyView = styled.section`
+const StVoteListWrapper = styled.main`
+  margin-left: 2.1rem;
+  margin-top: 5.1rem;
+
+  > h1 {
+    margin: 0rem 0rem 1.3rem 0rem;
+    color: ${({ theme }) => theme.colors.Pic_Color_Gray_Black};
+    ${({ theme }) => theme.fonts.Pic_Title2_Pretendard_Bold_20};
+  }
+`;
+
+const StVoteList = styled.main`
+  display: flex;
+`;
+const StVoteItem = styled.section`
+  display: flex;
+
+  width: 25.7rem;
+  height: 15.4rem;
+  margin-right: 1.6rem;
+
+  border-radius: 1.2rem;
+  background-color: ${({ theme }) => theme.colors.Pic_Color_Gray_2};
+`;
+
+const StVoteData = styled.article`
+  width: 10.3rem;
+  border-radius: 1.2rem 0rem 0rem 1.2rem;
+
+  > h1 {
+    padding: 1.3rem 1.6rem 0.7rem 1.4rem;
+
+    color: ${({ theme }) => theme.colors.Pic_Color_White};
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1.6rem;
+    line-height: 1.9rem;
+  }
+  > h2 {
+    padding: 0rem 1.6rem 5.8rem 1.4rem;
+
+    color: ${({ theme }) => theme.colors.Pic_Color_White};
+    ${({ theme }) => theme.fonts.Pic_Caption1_Pretendard_Semibold_12};
+    opacity: 0.5;
+  }
+  > h3 {
+    padding: 0rem 1.6rem 0rem 1.4rem;
+
+    color: ${({ theme }) => theme.colors.Pic_Color_Gray_4};
+    ${({ theme }) => theme.fonts.Pic_Caption1_Pretendard_Semibold_12};
+  }
+`;
+
+const StPreviewImg = styled.div`
+  width: 15.4rem;
+
+  border-top-right-radius: 1.2rem;
+  border-bottom-right-radius: 1.2rem;
+  background-color: ${({ theme }) => theme.colors.Pic_Color_Gray_Black};
+`;
+
+const StEmptyView = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -37,6 +119,6 @@ const StEmptyView = styled.section`
   }
   > p {
     color: ${({ theme }) => theme.colors.Pic_Color_Gray_2};
-    ${({ theme }) => theme.fonts.Pic_Body1_Pretendard_Medium_16}
+    ${({ theme }) => theme.fonts.Pic_Body1_Pretendard_Medium_16};
   }
 `;
