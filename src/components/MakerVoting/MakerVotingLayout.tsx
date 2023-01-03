202395842f586d5df310615bbfe31d85f99ebbb7
Dev-jwJeong <rgn95652@gmail.com>
2023-01-03 21:57:44 +0900

    style : 메이커 투표 생성 전체 레이아웃

---
+++ src/components/MakerVoting/MakerVotingLayout.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import ImageInput from './ImageInput';
+import TitleInput from './TitleInput';
+
+const MakerVotingLayout = () => {
+  const [input, setInput] = useState<string>('');
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <StMakerVotingLayoutWrapper>
+      <TitleInput input={input} handleChangeInput={handleChangeInput} />
+      <ImageInput input={input} />
+    </StMakerVotingLayoutWrapper>
+  );
+};
+
+export default MakerVotingLayout;
+
+const StMakerVotingLayoutWrapper = styled.section`
+  width: 100%;
+  padding: 0 2rem;
+`;
