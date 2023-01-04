28be2c19d2ada3c5965ec0e5c268554f97477311
seohee0112 <ghdtj7@naver.com>
2023-01-04 20:18:49 +0900

    style : 하단 메뉴바 아이콘 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,1 +1,5 @@
+export { ReactComponent as IcAngleMenu } from './angle_menu.svg';
+export { ReactComponent as IcBackgroundMenu } from './background_menu.svg';
+export { ReactComponent as IcFaceMenu } from './face_menu.svg';
+export { ReactComponent as IcJustMenu } from './just_menu.svg';
 export { ReactComponent as IcPickmeSticker } from './picme_sticker.svg';
