fac9629fd1df8cf60d5cc970673b54043987fde9
seohee0112 <ghdtj7@naver.com>
2023-01-04 20:19:08 +0900

    feat : DragReason 컴포넌트 구현

---
+++ src/components/Voting/player/index.ts
@@ -0,0 +1,1 @@
+export { default as DragReason } from './DragReason';
--- src/pages/Voting.tsx
+++ src/pages/Voting.tsx
@@ -4,32 +4,13 @@ import styled from 'styled-components';
 
 import { Error, Loading } from '../components/common';
 import { Header } from '../components/Voting';
+import { DragReason } from '../components/Voting/player';
 import { useGetVotingInfo } from '../lib/hooks/voting';
 import { votingStateSelector } from '../recoil/player/selector';
 
-const Voting = () => {
-  const { votingInfo, isLoading, isError } = useGetVotingInfo(1);
-  const [newVotingInfo, setNewVotingInfo] = useRecoilState(votingStateSelector);
-  useEffect(() => {
-    if (votingInfo) {
-      setNewVotingInfo(votingInfo.data);
-    }
-  }, []);
-
-  if (isLoading) return <Loading />;
-  if (isError) return <Error />;
-
-  return (
-    <div>
-      <p>{newVotingInfo.vote_title}</p>
-      <Header />
-      <StName>Happee의 사진을 골라죠💛</StName>
-    </div>
-  );
-};
+const Voting = () => (
+  <div>
+    <DragReason />
+  </div>
+);
 export default Voting;
-
-const StName = styled.p`
-  ${({ theme }) => theme.fonts.Pic_Body1_Pretendard_Medium_16}
-  ${({ theme }) => theme.fonts.Pic_Title2_Pretendard_Bold_20}
-`;
