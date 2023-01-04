456aedf2982bf86af618e005bf8723f00f6ea48a
Dev-jwJeong <rgn95652@gmail.com>
2023-01-05 05:09:14 +0900

    feat : 이미지 회전/완료 이미지 도출

--- src/components/MakerVoting/ImageCrop.tsx
+++ src/components/MakerVoting/ImageCrop.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Cropper from 'react-easy-crop';
-import { Point } from 'react-easy-crop/types';
+import { Area, Point } from 'react-easy-crop/types';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
@@ -8,14 +8,17 @@ import { votingImageState } from '../../recoil/maker/atom';
 
 interface ImageCropProps {
   firstCrop: boolean;
+  rotation: number;
+  setRotation: React.Dispatch<React.SetStateAction<number>>;
+  handleCropComplete: (croppedArea: Area, croppedAreaPixels: Area) => void;
 }
+
 const ImageCrop = (props: ImageCropProps) => {
   const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
   const [image, setImage] = useState('');
   const imageUrl = useRecoilValue(votingImageState);
   const [zoom, setZoom] = useState(1);
-
-  const { firstCrop } = props;
+  const { firstCrop, handleCropComplete, setRotation, rotation } = props;
 
   useEffect(() => {
     handleChangeImage();
@@ -34,10 +37,13 @@ const ImageCrop = (props: ImageCropProps) => {
         <Cropper
           image={image}
           crop={crop}
+          rotation={rotation}
           zoom={zoom}
           aspect={3 / 4}
           onCropChange={setCrop}
           onZoomChange={setZoom}
+          onRotationChange={setRotation}
+          onCropComplete={handleCropComplete}
           objectFit="horizontal-cover"
         />
       </StImageCropContainer>
