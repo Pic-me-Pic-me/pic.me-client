5d689e58fdb979bfc820084459c31c9190c55a3f
seohee0112 <ghdtj7@naver.com>
2023-01-05 19:05:17 +0900

    style : 헤더 로고 svg 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,6 +1,7 @@
 export { ReactComponent as IcAngleSticker } from './ic_angle.svg';
 export { ReactComponent as IcBackGroundSticker } from './ic_back_ground.svg';
 export { ReactComponent as IcFaceSticker } from './ic_face.svg';
+export { ReactComponent as IcHeaderLogo } from './ic_header_logo.svg';
 export { ReactComponent as IcJustSticker } from './ic_just.svg';
 export { ReactComponent as IcGoback } from './IcGoback.svg';
 export { ReactComponent as IcPickmeSticker } from './picme_sticker.svg';
