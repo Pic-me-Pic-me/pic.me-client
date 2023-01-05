a00c45297bcd89308c5995cea509d1fd0c31bde0
pinktopaz <heather128@ewhain.net>
2023-01-05 14:55:30 +0900

    fix : conflict 해결

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,10 +1,4 @@
-<<<<<<< Updated upstream
-=======
 export { ReactComponent as IcAfterCheckbox } from './checkbox_after.svg';
 export { ReactComponent as IcBeforeCheckbox } from './checkbox_before.svg';
 export { ReactComponent as IcDelete } from './delete_btn.svg';
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 export { ReactComponent as IcPickmeSticker } from './picme_sticker.svg';
