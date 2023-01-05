504b969d76a7328c5e9d808a670bc12354d7c754
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-01-05 21:16:52 +0900

    feat : 슬라이더 초기구조 세팅

--- src/components/Home/CurrentVoteDetail.tsx
+++ src/components/Home/CurrentVoteDetail.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { IcVoteShareBtn } from '../../asset/icon';
 import { ImgSiru } from '../../asset/image';
+import { useCarouselSize } from '../../lib/hooks/useCarouselSize';
+import { modifySliderRange, picmeSliderEvent } from '../../utils/picmeSliderEvent';
 import { HeaderLayout } from '../Layout';
 
 const CurrentVoteDetail = () => {
   const navigate = useNavigate();
+
+  const [currentIdx, setCurrentIdx] = useState<number>(0);
+  const [transX, setTransX] = useState<number>(0);
+  const { ref, width } = useCarouselSize();
 
   const handleGoHome = () => {
     navigate('/');
@@ -15,6 +22,9 @@ const CurrentVoteDetail = () => {
   const handleGoShare = () => {
     navigate('/share');
   };
+
+  const ImgList: JSX.Element[] = [<ImgSiru key="siruone" />, <ImgSiru key="sirutwo" />];
+
   return (
     <>
       <HeaderLayout HeaderTitle="현재 진행 중인 투표" handleGoback={handleGoHome} />
@@ -25,7 +35,49 @@ const CurrentVoteDetail = () => {
           <span>15명 투표 중</span>
           <span>12표</span>
         </StVoteStatus>
-        <img src={ImgSiru} alt="투표현황 사진" />
+        <StImgUl
+          currentIdx={currentIdx}
+          dragItemWidth={275}
+          transX={transX}
+          {...picmeSliderEvent({
+            onDragChange: (deltaX) => {
+              setTransX(modifySliderRange(deltaX, -width, width));
+            },
+            onDragEnd: (deltaX) => {
+              const maxIndex = ImgList.length - 1;
+              Array(2)
+                .fill(0)
+                .map((v, i) => 2 - i)
+                .some((num) => {
+                  if (deltaX < 70 * num) {
+                    setCurrentIdx(modifySliderRange(currentIdx + num, 0, maxIndex));
+                    return true;
+                  }
+                  if (deltaX > 70 * num) {
+                    setCurrentIdx(modifySliderRange(currentIdx - num, 0, maxIndex));
+                    return true;
+                  }
+                });
+
+              setTransX(0);
+            },
+          })}>
+          <li>
+            <img src={ImgSiru} alt="투표현황 사진" />
+          </li>
+          <li>
+            <img src={ImgSiru} alt="투표현황 사진" />
+          </li>
+          {/* {ImgList.map((menu, idx) =>
+            idx !== currentIdx ? (
+              <li key={idx} className="select_item">
+                {menu}
+              </li>
+            ) : (
+              <li key={idx}>{menu}</li>
+            ),
+          )} */}
+        </StImgUl>
         <IcVoteShareBtn onClick={handleGoShare} />
         <StCompleteVoteBtn>투표 마감</StCompleteVoteBtn>
       </CurrentVoteDetailWrapper>
@@ -40,6 +92,8 @@ const CurrentVoteDetailWrapper = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  overflow: hidden;
 
   & > span {
     ${({ theme }) => theme.fonts.Pic_Caption1_Pretendard_Semibold_12};
@@ -58,15 +112,6 @@ const CurrentVoteDetailWrapper = styled.section`
     height: 5.6rem;
 
     ${({ theme }) => theme.fonts.Pic_Title3_Pretendard_Bold_22}
-  }
-
-  & > img {
-    margin-top: 1.9rem;
-
-    width: 32.5rem;
-    height: 43.4rem;
-
-    border-radius: 1.2rem;
   }
 
   & > svg {
@@ -109,11 +154,41 @@ const StVoteStatus = styled.div`
   }
 `;
 
+const StImgUl = styled.ul<{ currentIdx: number; dragItemWidth: number; transX: number }>`
+  display: flex;
+  gap: 1.3rem;
+
+  padding-left: 2rem;
+  width: 43rem;
+
+  ${({ currentIdx, dragItemWidth, transX }) =>
+    css`
+      transform: translateX(${(-currentIdx * dragItemWidth + transX) / 10}rem);
+    `};
+  ${({ transX }) =>
+    css`
+      transition: transform ${transX ? 0 : 200}ms ease-in -out 3s;
+    `};
+  /* & > .select_item {
+    opacity: 0.5;
+  } */
+
+  & > li > img {
+    margin-top: 1.9rem;
+
+    width: 32.5rem;
+    height: 43.4rem;
+
+    border-radius: 1.2rem;
+  }
+`;
+
 const StCompleteVoteBtn = styled.button`
   width: 39rem;
   height: 6rem;
 
   margin-top: 4.7rem;
+  padding: 0;
 
   color: ${({ theme }) => theme.colors.Pic_Color_White};
   background-color: ${({ theme }) => theme.colors.Pic_Color_Coral};
