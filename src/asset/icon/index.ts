262104ead3e1f776cca6f92502008472b268567d
parkhyeonji <iamphj3@naver.com>
2023-01-06 00:17:43 +0900

    style : 버튼 스타일링

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,1 +1,2 @@
 export { ReactComponent as IcPickmeSticker } from './picme_sticker.svg';
+export { ReactComponent as IcModalLogo } from './IcModalLogo.svg';
--- src/pages/PlayerLanding.tsx
+++ src/pages/PlayerLanding.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { IcModalLogo } from '../asset/icon';
+
 const PlayerLanding = () => (
   <StModalWrapper>
     <StModal>
+      <IcModalLogo />
       <StButtonWrapper>
         <button type="button">익명 투표 시작하기</button>
         <button type="button">홈으로 가기</button>
@@ -35,14 +38,41 @@ const StModal = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
+
+  padding-top: 3.8rem;
   margin: auto;
-  width: 31rem;
-  height: 19.6rem;
+  width: 30.7rem;
+  height: 42.3rem;
+
   background-color: ${({ theme }) => theme.colors.Pic_Color_White};
   border-radius: 1rem;
+
+  & > svg {
+    width: 20rem;
+    height: 6.6rem;
+  }
 `;
 
-const StButtonWrapper = styled.button`
-  width: 25.3rem;
-  height: 5.4rem;
+const StButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.7rem;
+
+  padding-bottom: 2.1rem;
+
+  & > button {
+    width: 25.3rem;
+    height: 5.4rem;
+
+    border: none;
+    border-radius: 0.9rem;
+    background-color: ${({ theme }) => theme.colors.Pic_Color_Gray_Black};
+    color: ${({ theme }) => theme.colors.Pic_Color_White};
+    ${({ theme }) => theme.fonts.Pic_Body1_Pretendard_Medium_16}
+
+    :last-child {
+      background-color: ${({ theme }) => theme.colors.Pic_Color_Gray_4};
+    }
+  }
 `;
