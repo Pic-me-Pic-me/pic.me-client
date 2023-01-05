bdc065492506a786f92f701cba6e7ed41d4dfa42
pinktopaz <heather128@ewhain.net>
2023-01-05 14:42:12 +0900

    chore : 삭제 아이콘 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,1 +1,7 @@
+<<<<<<< Updated upstream
+=======
+export { ReactComponent as IcAfterCheckbox } from './checkbox_after.svg';
+export { ReactComponent as IcBeforeCheckbox } from './checkbox_before.svg';
+export { ReactComponent as IcDelete } from './delete_btn.svg';
+>>>>>>> Stashed changes
 export { ReactComponent as IcPickmeSticker } from './picme_sticker.svg';
