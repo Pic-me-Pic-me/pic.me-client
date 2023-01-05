35861600738c3e75d9765f056b6e2c59ef88cdc8
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-01-05 16:26:22 +0900

    fix : conflict 해결

--- src/asset/image/index.ts
+++ src/asset/image/index.ts
@@ -1,3 +1,3 @@
 export { default as FaceSticker } from './face_sticker.png';
-export { default as ImgSiru } from './ImgSiru.jpg';
 export { ReactComponent as ImgShareCapture } from './ImgShareCapture.svg';
+export { default as ImgSiru } from './ImgSiru.jpg';
