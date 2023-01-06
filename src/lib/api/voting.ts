2a2ca6a59cbc4576ad206f2ab2facf9218dec4c0
parkhyeonji <iamphj3@naver.com>
2023-01-06 12:14:53 +0900

    chore : observer 테스트

--- src/lib/api/voting.ts
+++ src/lib/api/voting.ts
@@ -2,3 +2,5 @@ import { client } from '../axios';
 import { VotingInfo } from './../../types/voting';
 
 export const postVoteData = (votingData: VotingInfo, token?: string) => client.post('/vote', votingData);
+
+export const getCurrentVoteData = (votingData: VotingInfo, token?: string) => client.get('/vote/current');
