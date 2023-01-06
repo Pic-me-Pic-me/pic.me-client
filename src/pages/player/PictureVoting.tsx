70ce36393731cdfe51eb265b8131222a235da453
seohee0112 <ghdtj7@naver.com>
2023-01-06 15:57:42 +0900

    feat : 투표 이미지 가져오기 서버 통신 구현

--- src/pages/player/PictureVoting.tsx
+++ src/pages/player/PictureVoting.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useRecoilState, useResetRecoilState } from 'recoil';
 
 import { IcHeaderLogo } from '../../asset/icon';
+import { Error, Loading } from '../../components/common';
 import { HeaderLayout } from '../../components/Layout';
 import { VotingLayout } from '../../components/Layout/player';
 import PictureSlider from '../../components/Voting/player/PictureSlider';
+import { useGetVotingInfo } from '../../lib/hooks/useGetVotingInfo';
+import { votingInfoState } from '../../recoil/player/atom';
 
 const PictureVoting = () => {
+  const { voteid } = useParams();
+  const { votingInfo, isLoading, isError } = useGetVotingInfo(Number(voteid));
+  const [votingInfoAtom, setVotingInfoState] = useRecoilState(votingInfoState);
+
   const navigate = useNavigate();
   const handleVotingSuccess = async () => {
-    navigate('/player/voting');
+    navigate('/player/reason_voting');
   };
   const handlePrevpage = () => {
     navigate('/player');
   };
+  useEffect(() => {
+    if (votingInfo?.data.data) {
+      setVotingInfoState(votingInfo.data.data);
+    }
+  }, [votingInfo]);
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
   return (
     <div>
       <HeaderLayout HeaderTitle={<IcHeaderLogo />} handleGoback={handlePrevpage} />
 
       <VotingLayout
-        votingTitle="햅히 체고"
+        votingTitle={votingInfoAtom.voteTitle}
         btnTitle="이 사진으로 하기"
         isActiveBtn={true}
         handlePlayer={handleVotingSuccess}>
