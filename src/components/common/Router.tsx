28b1cd09b9fb0ca36f3ff05c89df4161374b8dc8
seohee0112 <ghdtj7@naver.com>
2023-01-06 10:12:53 +0900

    feat : 중첩라우팅 및 params 로 voteid가져오기 셋팅

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -6,7 +6,7 @@ import Home from '../../pages/Home';
 import Library from '../../pages/Library';
 import Login from '../../pages/Login';
 import MakerLanding from '../../pages/MakerLanding';
-import { PictureVoting, StickerAttachment } from '../../pages/player';
+import { PictureVoting, ReasonVoting, StickerAttachment } from '../../pages/player';
 import PlayerLanding from '../../pages/PlayerLanding';
 import Signup from '../../pages/Signup';
 import Voting from '../../pages/Voting';
@@ -23,8 +23,9 @@ const Router = () => (
         <Route path="/makerlanding" element={<MakerLanding />} />
         <Route path="/voting" element={<Voting />} />
         <Route path="/player">
-          <Route path="picture_voting" element={<PictureVoting />} />
-          <Route path="voting" element={<StickerAttachment />} />
+          <Route path="picture_voting/:voteid" element={<PictureVoting />} />
+          <Route path="reason_voting" element={<ReasonVoting />} />
+          <Route path="sticker_voting" element={<StickerAttachment />} />
         </Route>
 
         <Route path="*" element={<Error404 />} />
