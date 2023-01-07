ac62c9452a75cdb7e133609bc51b10c0d71eb7f2
seohee0112 <ghdtj7@naver.com>
2023-01-07 14:26:28 +0900

    fix : 서버대로 데이터 타입 수정

--- src/types/voting.ts
+++ src/types/voting.ts
@@ -2,18 +2,16 @@ import { PicMeResponse } from './api';
 
 // 서버에서 pictures라고 지정해서,,우리는 복수명 안되는데! 어쩔수 없었오!
 export interface VotingInfo {
-  voteId: number;
-  voteStatus: boolean;
-  voteTitle: string;
-  picture: PictureInfo[];
+  vote_id: number;
+  vote_status: boolean;
+  vote_title: string;
+  Picture: PictureInfo[];
 }
 
 export interface PictureInfo {
   id: number;
   url: string;
-  isSelect?: boolean;
 }
-
 export interface StickerLocation {
   x: number;
   y: number;
