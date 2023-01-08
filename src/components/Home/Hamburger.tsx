075f7a398ac20c741646db70ef85fc4a998e2e1c
parkhyeonji <iamphj3@naver.com>
2023-01-08 17:39:20 +0900

    style : 햄버거 열렸을 때 배경 어둡게 처리

--- src/components/Home/Hamburger.tsx
+++ src/components/Home/Hamburger.tsx
@@ -32,19 +32,31 @@ const Hamburger = (props: HamburgerProps) => {
   };
 
   return (
-    <StOutsideHamburger>
-      <StHamburgerWrapper isOpen={isOpen}>
-        <StHamburgerMenu>회원 정보</StHamburgerMenu>
-        <StHamburgerMenu onClick={handleNavigateLibrary}>라이브러리</StHamburgerMenu>
-        <StHamburgerMenu>픽미 팀소개</StHamburgerMenu>
-      </StHamburgerWrapper>
-    </StOutsideHamburger>
+    <>
+      <StOutsideHamburger isOpen={isOpen}>
+        <StHamburgerWrapper isOpen={isOpen}>
+          <StHamburgerMenu>회원 정보</StHamburgerMenu>
+          <StHamburgerMenu onClick={handleNavigateLibrary}>라이브러리</StHamburgerMenu>
+          <StHamburgerMenu>픽미 팀소개</StHamburgerMenu>
+        </StHamburgerWrapper>
+      </StOutsideHamburger>
+    </>
   );
 };
 
 export default Hamburger;
 
-const StOutsideHamburger = styled.div``;
+const StOutsideHamburger = styled.div<{ isOpen?: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+
+  background-color: ${(props) => (props.isOpen ? 'rgba(0, 0, 0, 0.7)' : 'rgba(0, 0, 0, 0)')};
+`;
 
 const StHamburgerWrapper = styled.ul<{ isOpen?: boolean }>`
   position: fixed;
