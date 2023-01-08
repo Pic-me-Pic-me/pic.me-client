f360d11ec1c1812c6975f6364d0cf23a65ccb42b
pinktopaz <heather128@ewhain.net>
2023-01-08 22:51:44 +0900

    chore : pull 병합 오류 해결

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,4 +1,3 @@
-export { ReactComponent as IcAfterCheckbox } from './checkbox_after.svg';
-export { ReactComponent as IcBeforeCheckbox } from './checkbox_before.svg';
 export { ReactComponent as IcDelete } from './delete_btn.svg';
+export { ReactComponent as IcGoback } from './IcGoback.svg';
 export { ReactComponent as IcPickmeSticker } from './picme_sticker.svg';
