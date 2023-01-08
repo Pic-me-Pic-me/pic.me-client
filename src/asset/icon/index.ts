6ad75c485514324c2736b5c01641b1034e55798d
pinktopaz <heather128@ewhain.net>
2023-01-09 03:46:31 +0900

    chore : 버튼 에셋 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -4,3 +4,5 @@ export { ReactComponent as IcJust } from './ic_just.svg';
 export { ReactComponent as IcMood } from './ic_mood.svg';
 export { ReactComponent as IcGoback } from './IcGoback.svg';
 export { ReactComponent as IcPickmeSticker } from './picme_sticker.svg';
+export { ReactComponent as IcReasonBtnAfter } from './ReasonBtn_after.svg';
+export { ReactComponent as IcReasonBtnBefore } from './ReasonBtn_before.svg';
