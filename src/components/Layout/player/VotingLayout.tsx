b8374d993edbb9f46bf5b6934f4438937c580e5c
seohee0112 <ghdtj7@naver.com>
2023-01-09 01:50:04 +0900

    feat : 로고 이동 및 스크롤바

--- src/components/Layout/player/VotingLayout.tsx
+++ src/components/Layout/player/VotingLayout.tsx
@@ -50,7 +50,13 @@ const StPlayerVotingWrapper = styled.div<{ pageType: string }>`
   flex-direction: column;
 
   width: 100%;
-
+  & {
+    -ms-overflow-style: none; /* 인터넷 익스플로러 */
+    scrollbar-width: none; /* 파이어폭스 */
+  }
+  & > div::-webkit-scrollbar {
+    display: none; /* 크롬, 사파리, 오페라, 엣지 */
+  }
   & > header,
   footer {
     display: flex;
