18ce159e443d69b727a04d0923a045440abb8d81
Dev-jwJeong <rgn95652@gmail.com>
2023-01-09 23:10:09 +0900

    style : Error404페이지 컴포넌트

--- src/components/common/Error.tsx
+++ src/components/common/Error.tsx
@@ -1,5 +1,58 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Error = () => <div>Error!!!</div>;
+import { IcError } from '../../asset/icon';
+
+const Error = () => {
+  const navigate = useNavigate();
+  return (
+    <StErrorWrapper>
+      <IcError />
+      <StWarningBlock>
+        <StWarningText>해당 페이지를 찾을 수 없어요.</StWarningText>
+        <StWarningText>올바른 URL을 입력했는지 확인해주세요!</StWarningText>
+      </StWarningBlock>
+      <StBackHomeBtn type="button" onClick={() => navigate('/')}>
+        홈으로 돌아가기
+      </StBackHomeBtn>
+    </StErrorWrapper>
+  );
+};
 
 export default Error;
+
+const StErrorWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100%;
+  padding: 0 2rem;
+`;
+const StWarningBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 3.429rem;
+`;
+const StWarningText = styled.span`
+  color: ${({ theme }) => theme.colors.Pic_Color_Gray_4};
+  ${({ theme }) => theme.fonts.Pic_Subtitle2_Pretendard_Medium_18};
+`;
+const StBackHomeBtn = styled.button`
+  width: 100%;
+  height: 6rem;
+  margin-top: 18.1rem;
+
+  border: none;
+  border-radius: 0.9rem;
+  background-color: ${({ theme }) => theme.colors.Pic_Color_Coral};
+  color: ${({ theme }) => theme.colors.Pic_Color_White};
+  ${({ theme }) => theme.fonts.Pic_Body1_Pretendard_Medium_16};
+
+  cursor: pointer;
+`;
