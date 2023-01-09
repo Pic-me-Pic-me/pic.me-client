f7b1dd64a2bb0eb03bc7ee67bd642214b72c7e3c
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-01-09 19:57:39 +0900

    chore : atom 파일 초기화

--- src/recoil/maker/atom.ts
+++ src/recoil/maker/atom.ts
@@ -1,29 +1,5 @@
-import { PictureProps, VoteInfoProps } from './../../types/voting';
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
 //페이지가 변경되더라도 상태관리를 유지
 const { persistAtom } = recoilPersist();
-
-export const currentVoteInfoState = atom<VoteInfoProps>({
-  key: 'currnetVoteInfo',
-  default: {
-    voteId: 0,
-    voteStatus: true,
-    voteTitle: '',
-    currentVote: 0,
-    // createDate: new Date(0),
-    Picture: [],
-  },
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const pictureInfoState = atom<PictureProps>({
-  key: 'pictureInfo',
-  default: {
-    pictureId: 0,
-    url: '',
-    count: 0,
-    Sticker: [],
-  },
-});
