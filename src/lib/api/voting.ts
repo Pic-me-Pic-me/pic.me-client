1095cf9fe4817dd65918af6c5070f38182f18fa5
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-01-10 01:18:21 +0900

    chore : 주석 제거

--- src/lib/api/voting.ts
+++ src/lib/api/voting.ts
@@ -8,24 +8,10 @@ export const postVoteData = (votingData: VotingInfo, token?: string) => client.p
 export const getCurrentVoteData = async (voteid: string | undefined) => {
   const { data } = await client.get(`/vote/maker/singleResult/${voteid}`);
   if (data.status === 200) {
+    console.log(data.data);
     return data.data;
   }
 };
-
-// export interface ResponseDto {
-//   message: string;
-//   status: number;
-//   success: boolean;
-// }
-
-// export interface Response<T> extends ResponseDto {
-//   data: { data: T };
-// }
-
-// export const useGetCurrentVoteData = (voteid: string | undefined) => {
-//   const { data } = useSWR(`/vote/maker/singleResult/${voteid}`, client.get);
-//   return data;
-// };
 
 export const patchCurrentVoteData = async (voteid: string | undefined) => {
   const res = await client.patch(`/vote/close/${voteid}`);
