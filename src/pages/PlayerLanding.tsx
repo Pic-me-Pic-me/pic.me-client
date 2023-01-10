7c67738e2eece593009f9b38897d689db77bd144
seohee0112 <ghdtj7@naver.com>
2023-01-10 11:20:08 +0900

    fix : 익명 재투표 허용

--- src/pages/PlayerLanding.tsx
+++ src/pages/PlayerLanding.tsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
 
 import { Error, Loading } from '../components/common';
 import { FinishedLanding, VoteLanding } from '../components/Landing';
 import { useGetVotingInfo } from '../lib/hooks/useGetVotingInfo';
-import { votingInfoState } from '../recoil/player/atom';
+import { stickerInfoState, votingInfoState } from '../recoil/player/atom';
 
 const PlayerLanding = () => {
   const { voteId } = useParams<{ voteId: string }>();
+
   const { votingInfo, isLoading, isError } = useGetVotingInfo(Number(voteId));
+
   const setVotingInfoState = useSetRecoilState(votingInfoState);
+  const resetStickerInfoState = useResetRecoilState(stickerInfoState);
+
   useEffect(() => {
     if (votingInfo?.data) {
+      resetStickerInfoState();
       setVotingInfoState({
         ...votingInfo?.data,
       });
     }
-  }, []);
+  }, [votingInfo]);
   if (isLoading) return <Loading />;
   if (isError) return <Error />;
 
