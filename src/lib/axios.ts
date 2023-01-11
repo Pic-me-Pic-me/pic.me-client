4ce3f5569b5ca678990711dca84488021fa0cfe3
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-01-12 02:54:02 +0900

    hotfix : 라이브러리 기능 이식

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,7 +5,7 @@ export const client = axios.create({
   headers: {
     'Content-type': 'application/json',
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1NiwiaWF0IjoxNjczNDEyNjcxLCJleHAiOjE2NzM0MTQ0NzF9.1cNaIuDO9MJ676oK1mBpqNV_eMXfVkdAx-Gf5fSGYr8',
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ2LCJpYXQiOjE2NzM0NTk1MzEsImV4cCI6MTY3MzQ2MTMzMX0.ejzB7hUYJ8qI_1t7xNPp9xufbvUwd032BhGH_RHnPwo',
   },
 });
 
