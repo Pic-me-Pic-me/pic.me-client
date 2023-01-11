bf0f2aa2eb6d4b6924676ce9f54f896577ad27ed
seohee0112 <ghdtj7@naver.com>
2023-01-12 04:52:00 +0900

    feat : 결과 페이지 구현

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import styled from 'styled-components';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import styled, { css } from 'styled-components';
 
+import { IcGoback, IcResultLeft, IcResultRight } from '../asset/icon';
 import resultSample from '../asset/image/resultSample.png';
 import { Error, Loading } from '../components/common';
 import { HeaderLayout } from '../components/Layout';
@@ -9,28 +11,54 @@ import ResultPicture from '../components/Result/ResultPicture';
 import ResultReason from '../components/Result/ResultReason';
 import SliderTitle from '../components/Result/SliderTitle';
 import useGetVoteResult from '../lib/hooks/useGetVoteResult';
+import { stickerResultState } from '../recoil/maker/atom';
+import { stickerCountSelector } from '../recoil/maker/selector';
+import { MakerVoteInfo } from '../types/vote';
+import { jsonGetStickerList } from '../utils/jsonGetStickerList';
 
 export const Result = () => {
   const navigate = useNavigate();
   const { voteId } = useParams() as { voteId: string };
   const { voteResult, isLoading, isError } = useGetVoteResult(voteId);
+  const [chosenPictureIdx, setChosenPictureIdx] = useState(0);
+  const [isOpenResultReason, setIsOpenResultReason] = useState(false);
+  const setStickerResultState = useSetRecoilState(stickerResultState);
+  const stickerCount = useRecoilValue(stickerCountSelector);
 
+  const handleIsOpenResultReason = () => {
+    setIsOpenResultReason(!isOpenResultReason);
+  };
+
+  useEffect(() => {
+    if (voteResult) {
+      setStickerResultState(jsonGetStickerList(voteResult.Picture[chosenPictureIdx].Sticker));
+    }
+  }, [chosenPictureIdx]);
   if (isLoading) <Loading />;
-  if (isError) <Error />;
 
   if (voteResult)
     return (
       <>
-        <StBackgroundWrapper src={voteResult.Picture[1].url}>
-          <StBackground>
+        <StBackgroundWrapper src={voteResult.Picture[chosenPictureIdx].url}>
+          <StBackground isChosenPic={!chosenPictureIdx}>
             <HeaderLayout HeaderTitle="최종 투표 결과" handleGoback={() => navigate(-1)} isBanner></HeaderLayout>
-
+            {!chosenPictureIdx ? (
+              <IcResultRight onClick={() => setChosenPictureIdx(1)} />
+            ) : (
+              <IcResultLeft onClick={() => setChosenPictureIdx(0)} />
+            )}
             <SliderTitle
-              isChosenPic={false}
+              isChosenPic={!chosenPictureIdx}
               voteTitle={voteResult.voteTitle}
-              voteTotalNumber={voteResult.currentVote}></SliderTitle>
-            <ResultPicture src={voteResult.Picture[1].url}></ResultPicture>
-            {/* <ResultReason totalVote={voteResult.currentVote} bestReasonVote={}></ResultReason> */}
+              voteTotalNumber={stickerCount}></SliderTitle>
+
+            <section>
+              <ResultPicture src={voteResult.Picture[chosenPictureIdx].url}></ResultPicture>
+              <ResultReason
+                handleIsOpenResultReason={handleIsOpenResultReason}
+                isOpenResultReason={isOpenResultReason}
+              />
+            </section>
           </StBackground>
         </StBackgroundWrapper>
       </>
@@ -44,11 +72,33 @@ const StBackgroundWrapper = styled.div<{ src: string }>`
   background-size: cover;
 `;
 
-const StBackground = styled.main`
-  height: 100vh%;
+const StBackground = styled.main<{ isChosenPic: boolean }>`
+  height: 100vh;
 
   background-color: rgba(0, 0, 0, 0.5);
   backdrop-filter: blur(15px);
 
   z-index: -5;
+
+  & > svg {
+    width: 2.9rem;
+    position: absolute;
+
+    top: ${window.screen.height * 0.05}rem;
+    ${({ isChosenPic }) =>
+      isChosenPic
+        ? css`
+            right: 0;
+          `
+        : css`
+            left: 0;
+          `};
+
+    z-index: 2;
+  }
+  & > section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
