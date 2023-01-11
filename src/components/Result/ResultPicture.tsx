c85338d79cfc65ccfc556647dd56cdd67cc3baab
seohee0112 <ghdtj7@naver.com>
2023-01-12 04:49:18 +0900

    feat : 결과 보기 스티커 붙이기

--- src/components/Result/ResultPicture.tsx
+++ src/components/Result/ResultPicture.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
 import { IcReasonBtnBefore } from '../../asset/icon';
-import { HeaderLayout } from '../Layout';
+import { STICKER_LIST } from '../../constant/StickerIconList';
+import { stickerResultState } from '../../recoil/maker/atom';
+import { NaturalImgInfo, StickerResultInfo } from '../../types/vote';
 
 interface ReasonPicProps {
   src: string;
 }
 const ResultPicture = (props: ReasonPicProps) => {
   const { src } = props;
+  const [imgInfo, setImgInfo] = useState<NaturalImgInfo>();
+  const stickerResult = useRecoilValue(stickerResultState);
+
+  const handleImgSize = (e: React.SyntheticEvent) => {
+    const { naturalWidth, naturalHeight } = e.target as HTMLImageElement;
+    setImgInfo({ width: naturalWidth, height: naturalHeight });
+  };
 
   return (
     <>
       <StPictureWrapper>
-        <StPicture src={src}></StPicture>
-        <IcReasonBtnBefore />
+        <StPicture src={src} onLoad={handleImgSize} />
+        {stickerResult.map(({ stickerLocation, emoji }, idx) =>
+          stickerLocation.map(({ x, y, degRate }, stickerIdx) => (
+            <StEmojiIconWrapper key={`sticker${stickerIdx}_${emoji}`} locationX={x} locationY={y} degRate={degRate}>
+              {STICKER_LIST[emoji].icon()}
+            </StEmojiIconWrapper>
+          )),
+        )}
       </StPictureWrapper>
     </>
   );
@@ -27,7 +43,9 @@ const StPictureWrapper = styled.div`
   align-items: center;
   position: relative;
 
+  width: 87%;
   margin-top: 2.3rem;
+  overflow: hidden;
 
   > svg {
     position: absolute;
@@ -36,8 +54,24 @@ const StPictureWrapper = styled.div`
 `;
 
 const StPicture = styled.img<{ src: string }>`
-  width: 36.6rem;
+  width: 100%;
   height: 48.836rem;
-`;
 
+  object-fit: cover;
+`;
+const StEmojiIconWrapper = styled.div<{ locationX: number; locationY: number; degRate: number }>`
+  position: absolute;
+  left: ${({ locationX }) => locationX}rem;
+  top: ${({ locationY }) => locationY}rem;
+  & > svg {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 5.3rem;
+    height: 5.3rem;
+    z-index: 3;
+    transform-origin: 50% 50%;
+    transform: ${({ degRate }) => `rotate(${degRate}deg)`};
+  }
+`;
 export default ResultPicture;
