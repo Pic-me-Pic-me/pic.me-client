cfd93a054ee4025d1d7dcf0f4e4c17350f433372
seohee0112 <ghdtj7@naver.com>
2023-01-12 04:51:24 +0900

    feat : 메이커 스티커 정보관련 atom 구현

--- src/recoil/maker/atom.ts
+++ src/recoil/maker/atom.ts
@@ -1,5 +1,19 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
+import { StickerResultInfo } from '../../types/vote';
+
 //페이지가 변경되더라도 상태관리를 유지
 const { persistAtom } = recoilPersist();
+
+export const stickerResultState = atom<StickerResultInfo[]>({
+  key: 'stickerResult',
+  default: [
+    {
+      stickerLocation: [],
+      emoji: 0,
+      count: 0,
+    },
+  ],
+  effects_UNSTABLE: [persistAtom],
+});
