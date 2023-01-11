c96d15fbef7dfb30b42ab704b211a4f5844fd33a
seohee0112 <ghdtj7@naver.com>
2023-01-12 04:50:22 +0900

    chore : 서버 통신에 필요한 타입 정의

--- src/types/vote.ts
+++ src/types/vote.ts
@@ -14,9 +14,36 @@ export interface MakerPictureData {
   count: number;
   Sticker: GetStickerResultInfo[];
 }
-
+export interface StickerLocation {
+  x: number;
+  y: number;
+  degRate: number;
+}
 export interface GetStickerResultInfo {
   stickerLocation: string;
   emoji: number;
   count: number;
+}
+export interface VoteReasonInfo {
+  totalVote: number;
+  bestReason: string;
+  bestReasonVote: number;
+  faceStickerCount: number;
+  angleStickerCount: number;
+  moodStickerCount: number;
+  jusStickerCount: number;
+}
+export interface StickerInfo {
+  pictureId: number;
+  location: StickerLocation[];
+  emoji: number;
+}
+export interface StickerResultInfo {
+  stickerLocation: StickerLocation[];
+  emoji: number;
+  count: number;
+}
+export interface NaturalImgInfo {
+  width: number;
+  height: number;
 }
