c152f7b1b40711a3f246c46a2e70dd2cd6bae93d
Dev-jwJeong <rgn95652@gmail.com>
2023-01-13 18:48:49 +0900

    fix : 투표 생성 이미지 변경

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -12,18 +12,6 @@ export interface ModalProps {
 
 const Modal = (props: ModalProps) => {
   const { isShowing, message, handleHide, handleConfirm, isFinishing, isDeleteUser } = props;
-  useEffect(() => {
-    document.body.style.cssText = `
-          position: fixed;
-          top: -${window.scrollY}px;
-          overflow-y: scroll;
-          width: 100%;`;
-    return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = '';
-      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-    };
-  }, []);
   return (
     <>
       {isShowing && (
