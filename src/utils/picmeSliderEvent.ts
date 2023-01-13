6f3df02787279261ff3eb80b7e40ae12eb095993
Dev-jwJeong <rgn95652@gmail.com>
2023-01-13 15:41:03 +0900

    chore : eslint 적용

--- src/utils/picmeSliderEvent.ts
+++ src/utils/picmeSliderEvent.ts
@@ -11,26 +11,24 @@ export const picmeSliderEvent = ({
   onDragChange?: (deltaX: number, deltaY: number) => void;
   onDragEnd?: (deltaX: number, deltaY: number) => void;
   stopPropagation?: boolean;
-}) => {
-  return {
-    onTouchStart: (touchEvent: React.TouchEvent<HTMLUListElement>) => {
-      if (stopPropagation) touchEvent.stopPropagation();
+}) => ({
+  onTouchStart: (touchEvent: React.TouchEvent<HTMLUListElement>) => {
+    if (stopPropagation) touchEvent.stopPropagation();
 
-      const touchMoveHandler = (moveEvent: TouchEvent) => {
-        const deltaX = moveEvent.touches[0].pageX - touchEvent.touches[0].pageX;
-        const deltaY = moveEvent.touches[0].pageY - touchEvent.touches[0].pageY;
-        onDragChange?.(deltaX, deltaY);
-      };
+    const touchMoveHandler = (moveEvent: TouchEvent) => {
+      const deltaX = moveEvent.touches[0].pageX - touchEvent.touches[0].pageX;
+      const deltaY = moveEvent.touches[0].pageY - touchEvent.touches[0].pageY;
+      onDragChange?.(deltaX, deltaY);
+    };
 
-      const touchEndHandler = (moveEvent: TouchEvent) => {
-        const deltaX = moveEvent.changedTouches[0].pageX - touchEvent.changedTouches[0].pageX;
-        const deltaY = moveEvent.changedTouches[0].pageY - touchEvent.changedTouches[0].pageY;
-        onDragEnd?.(deltaX, deltaY);
-        document.removeEventListener('touchmove', touchMoveHandler);
-      };
+    const touchEndHandler = (moveEvent: TouchEvent) => {
+      const deltaX = moveEvent.changedTouches[0].pageX - touchEvent.changedTouches[0].pageX;
+      const deltaY = moveEvent.changedTouches[0].pageY - touchEvent.changedTouches[0].pageY;
+      onDragEnd?.(deltaX, deltaY);
+      document.removeEventListener('touchmove', touchMoveHandler);
+    };
 
-      document.addEventListener('touchmove', touchMoveHandler, { passive: false });
-      document.addEventListener('touchend', touchEndHandler, { once: true });
-    },
-  };
-};
+    document.addEventListener('touchmove', touchMoveHandler, { passive: false });
+    document.addEventListener('touchend', touchEndHandler, { once: true });
+  },
+});
