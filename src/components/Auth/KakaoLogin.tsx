a835551d6f47e30480f5ec501e457793c0fcfb4d
seohee0112 <ghdtj7@naver.com>
2023-01-21 12:58:01 +0900

    docs : 앱키 노출 제거

--- src/components/Auth/KakaoLogin.tsx
+++ src/components/Auth/KakaoLogin.tsx
@@ -15,7 +15,6 @@ const KakaoLogin = () => {
 
   const initKakao = () => {
     if (Kakao && !Kakao.isInitialized()) {
-      console.log(process.env.REACT_APP_JS_KEY);
       Kakao.init(process.env.REACT_APP_JS_KEY);
     }
   };
