a74161ea593bd8d698936ce9d23b53a3cb823487
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-02-06 21:50:51 +0900

    feat : 사진 정보 atom 에 추가

--- src/recoil/maker/atom.ts
+++ src/recoil/maker/atom.ts
@@ -1,7 +1,7 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
-import { StickerResultInfo } from '../../types/vote';
+import { MakerPictureData, StickerResultInfo } from '../../types/vote';
 import { VotingImageInfo } from '../../types/votingImage';
 
 //페이지가 변경되더라도 상태관리를 유지
@@ -14,6 +14,19 @@ export const votingImageState = atom<VotingImageInfo>({
     firstImageUrl: '',
     secondImageUrl: '',
   },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const pictureResultState = atom<MakerPictureData[]>({
+  key: 'pictureResult',
+  default: [
+    {
+      Sticker: [],
+      count: 0,
+      pictureId: 0,
+      url: '',
+    },
+  ],
   effects_UNSTABLE: [persistAtom],
 });
 
