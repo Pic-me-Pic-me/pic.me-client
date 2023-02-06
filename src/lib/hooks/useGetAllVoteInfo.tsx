762c5215755e130f94c474c20a29064f5f6d5f43
pinktopaz <heather128@ewhain.net>
2023-02-06 22:52:02 +0900

    feat : 라이브러리 세로 스크롤 SWR 적용

---
+++ src/lib/hooks/useGetAllVoteInfo.tsx
@@ -0,0 +1,22 @@
+import { AxiosResponse } from 'axios';
+import useSWR from 'swr';
+
+import { EndedVoteInfo } from '../../types/library';
+import { picmeGetFetcher } from '../axios';
+
+//세로 무한스크롤
+export const useGetAllVoteInfo = (prevLastDate: number) => {
+  const { data, error } = useSWR<AxiosResponse<EndedVoteInfo[]>>(
+    `/vote/library/scroll/all?flag=${prevLastDate}`,
+    picmeGetFetcher,
+    {
+      errorRetryCount: 3,
+    },
+  );
+
+  return {
+    allVoteInfo: data?.data,
+    isLoading: !error && !data?.data,
+    isError: error,
+  };
+};
