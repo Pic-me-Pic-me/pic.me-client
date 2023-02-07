e17480a88cc6231d16184fe0d5268c58f3d9aaea
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2023-02-07 17:14:33 +0900

    chore : 불필요한 코드 제거

--- src/pages/CurrentVoteDetail.tsx
+++ src/pages/CurrentVoteDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useResetRecoilState, useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
