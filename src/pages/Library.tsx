9dfc9e4c86580de8dd85d97decca21c2daf900f2
pinktopaz <heather128@ewhain.net>
2023-02-08 00:41:07 +0900

    chore : empty 뷰 추가

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { IcEmptyLibrary } from '../asset/icon';
 import LandingLibrary from '../components/Landing/maker/LandingLibrary';
 import { HeaderLayout } from '../components/Layout';
 import MonthVoting from '../components/Library/MonthVoting';
@@ -13,15 +14,14 @@ import Error404 from './Error404';
 const Library = () => {
   const navigate = useNavigate();
   const [ref, inView] = useInView();
+  const { allVoteInfoList, isLoading, isError, size, setSize } = useGetAllVoteInfo();
 
-  const nextIndex = useRef(0);
-  const [monthVoteData, setMonthVoteData] = useState<EndedVoteInfo[]>([]);
-  const { allVoteInfo, isLoading, isError } = useGetAllVoteInfo(
-    monthVoteData[nextIndex.current] ? monthVoteData[nextIndex.current].date : 0,
-  );
-
-  useEffect(() => {
-    getMoreItem();
+  const getMoreItem = useCallback(async () => {
+    if (allVoteInfoList) {
+      setSize((prev) => prev + 1);
+    } else {
+      return;
+    }
   }, []);
 
   useEffect(() => {
@@ -29,17 +29,6 @@ const Library = () => {
       getMoreItem();
     }
   }, [inView]);
-
-  const getMoreItem = () => {
-    const getItem = allVoteInfo;
-
-    if (monthVoteData.length > 0 && getItem?.length === 0) return;
-    if (getItem) {
-      const newData = [...monthVoteData, ...getItem];
-      nextIndex.current = newData.length - 1;
-      setMonthVoteData(newData);
-    }
-  };
 
   if (isLoading) {
     return <LandingLibrary />;
@@ -56,18 +45,26 @@ const Library = () => {
           navigate('/home');
         }}
       />
-
-      <StMonthVotingInfo>
-        {monthVoteData.map((votingInfo: EndedVoteInfo, idx: number) =>
-          idx === monthVoteData.length - 1 ? (
-            <div key={idx} ref={ref}>
+      {allVoteInfoList.list.length !== 0 ? (
+        <StMonthVotingInfo>
+          {allVoteInfoList.list.map((votingInfo: EndedVoteInfo, idx: number) =>
+            idx === allVoteInfoList.list.length - 1 ? (
+              <div key={idx} ref={ref}>
+                <MonthVoting date={votingInfo.date} votes={votingInfo.votes} key={idx}></MonthVoting>
+              </div>
+            ) : (
               <MonthVoting date={votingInfo.date} votes={votingInfo.votes} key={idx}></MonthVoting>
-            </div>
-          ) : (
-            <MonthVoting date={votingInfo.date} votes={votingInfo.votes} key={idx}></MonthVoting>
-          ),
-        )}
-      </StMonthVotingInfo>
+            ),
+          )}
+        </StMonthVotingInfo>
+      ) : (
+        <StEmptyView>
+          <div>
+            <IcEmptyLibrary />
+            <p>아직 마감된 투표가 없어요!</p>
+          </div>
+        </StEmptyView>
+      )}
     </>
   );
 };
@@ -81,6 +78,24 @@ const StMonthVotingInfo = styled.article`
   margin-top: 3.3rem;
 
   overflow-y: scroll;
+`;
+
+const StEmptyView = styled.main`
+  div {
+    margin-top: 22.4rem;
+    margin-bottom: 43.7rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div > p {
+    margin-top: 3.429rem;
+    ${({ theme }) => theme.fonts.Pic_Subtitle2_Pretendard_Medium_18};
+    color: ${({ theme }) => theme.colors.Pic_Color_Gray_4};
+  }
 `;
 
 export default Library;
