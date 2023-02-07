e14627a0bfa3f75ab3a8f2affa0257b657938195
seohee0112 <ghdtj7@naver.com>
2023-02-07 15:19:19 +0900

    refactor : useSWRInfinite로 무한스크롤 기능 구현

--- src/lib/hooks/useGetCurrentVoteList.tsx
+++ src/lib/hooks/useGetCurrentVoteList.tsx
@@ -1,19 +1,49 @@
 import { AxiosResponse } from 'axios';
-import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
 
 import { VoteResultData } from '../../types/vote';
 import { picmeGetFetcher } from '../axios';
 
-const useGetCurrentVoteList = (cursorId: string) => {
-  const { data, error } = useSWR<AxiosResponse<VoteResultData>>(`vote/list/${cursorId}`, picmeGetFetcher, {
+const getKey = (cursorIdx: number, voteListData: AxiosResponse<VoteResultData>) => {
+  // console.log('getKey', cursorIdx, voteListData);
+
+  if (cursorIdx === 0) return `vote/list/${cursorIdx}`;
+  if (voteListData) return `vote/list/${voteListData.data.resCursorId}`;
+  return `vote/list/${cursorIdx}`;
+};
+const useGetCurrentVoteList = () => {
+  const { data, error, size, setSize } = useSWRInfinite<AxiosResponse<VoteResultData>>(getKey, picmeGetFetcher, {
     errorRetryCount: 3,
   });
-  console.log(cursorId, data);
+  // console.log('useGet', data);
+
+  const parseData = data?.map((item) => item.data);
+
+  // console.log('parseDaa', parseData);
+  // console.log(parseData?.map((item) => item.result));
+
+  const parseResultList = parseData
+    ?.map((item) => item.result)
+    .flat()
+    .filter((result) => result);
+
+  const parseCursorIdList = parseData
+    ?.map((item) => item.resCursorId)
+    .flat()
+    .filter((resCursorId) => resCursorId);
+
+  // console.log('parseResultList', parseResultList);
+  // console.log('parseCursorIdList', parseCursorIdList);
 
   return {
-    voteListResult: data?.data,
-    isLoading: !error && !data?.data,
+    voteListResult: {
+      result: parseResultList ? parseResultList : [],
+      resCursorId: parseCursorIdList ? parseCursorIdList[parseCursorIdList.length - 1] : '',
+    },
+    isLoading: !error && !data,
     isError: error,
+    size,
+    setSize,
   };
 };
 
