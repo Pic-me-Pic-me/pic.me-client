52019486f1591d74f680cbe3883789d7914d986d
pinktopaz <heather128@ewhain.net>
2023-02-08 21:13:36 +0900

    feat : 세로스크롤 SWRInfinite 적용

--- src/lib/hooks/useGetAllVoteInfo.tsx
+++ src/lib/hooks/useGetAllVoteInfo.tsx
@@ -1,22 +1,40 @@
 import { AxiosResponse } from 'axios';
-import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
 
 import { EndedVoteInfo } from '../../types/library';
 import { picmeGetFetcher } from '../axios';
 
 //세로 무한스크롤
-export const useGetAllVoteInfo = (prevLastDate: number) => {
-  const { data, error } = useSWR<AxiosResponse<EndedVoteInfo[]>>(
-    `/vote/library/scroll/all?flag=${prevLastDate}`,
+const getKey = (prevLastDate: number, allVoteInfo: AxiosResponse<EndedVoteInfo[]>) => {
+  if (prevLastDate === 0) {
+    return `/vote/library/scroll/all?flag=${prevLastDate}`;
+  }
+  if (allVoteInfo) {
+    return `/vote/library/scroll/all?flag=${allVoteInfo.data[allVoteInfo.data.length - 1].date}`;
+  }
+  return;
+};
+
+export const useGetAllVoteInfo = () => {
+  const { data, isLoading, error, size, setSize } = useSWRInfinite<AxiosResponse<EndedVoteInfo[]>>(
+    getKey,
     picmeGetFetcher,
     {
       errorRetryCount: 3,
     },
   );
 
+  const parseResultList = data?.map((item) => item.data).flat();
+
+  console.log(parseResultList);
+
   return {
-    allVoteInfo: data?.data,
-    isLoading: !error && !data?.data,
+    allVoteInfoList: {
+      list: parseResultList ? parseResultList : [],
+    },
+    isLoading,
     isError: error,
+    size,
+    setSize,
   };
 };
