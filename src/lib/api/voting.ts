b1353654bbbd72d5d994cf121a22fe27bf9b7772
Dev-jwJeong <rgn95652@gmail.com>
2023-02-08 16:03:06 +0900

    refactor : api 호출 함수 분리

--- src/lib/api/voting.ts
+++ src/lib/api/voting.ts
@@ -3,6 +3,8 @@ import { AxiosResponse } from 'axios';
 import { client } from '../axios';
 import { PlayerStickerInfo, VoteResultData } from './../../types/vote';
 
+const ACCESS_TOKEN = localStorage.getItem('accessToken');
+
 export const patchCurrentVoteData = async (voteid: string | undefined) => {
   const res = await client.patch(`/vote/close/${voteid}`);
   return res;
@@ -28,6 +30,20 @@ export const postStickerData = async (stickerInfo: PlayerStickerInfo) => {
   try {
     const { data } = await client.post('/sticker', stickerInfo);
     return data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const postImage = async (imageData: FormData) => {
+  try {
+    const data = await client.post(`/vote`, imageData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+    });
+    return data.data.status;
   } catch (e) {
     console.error(e);
   }
