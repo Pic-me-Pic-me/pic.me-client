cbb714c8b53e5ac193cf9d29b7130e2ebadd5fee
pinktopaz <heather128@ewhain.net>
2023-02-09 22:03:47 +0900

    feat : 가로 무한스크롤 서버 변경사항 적용

--- src/lib/hooks/useGetMonthlyLibraryInfo.tsx
+++ src/lib/hooks/useGetMonthlyLibraryInfo.tsx
@@ -7,15 +7,12 @@ import { picmeGetFetcher } from '../axios';
 export const useGetMonthlyLibraryInfo = (date: number) => {
   const { data, isLoading, error, size, setSize } = useSWRInfinite<AxiosResponse<VoteInfo[]>>(
     (idx: number, monthlyVoteInfo: AxiosResponse<VoteInfo[]>) => {
-      // date 받으려고 따로 getKey 선언한걸 뺐어!!!!
       if (idx === 0) return `/vote/library/scroll/month?flag=${0}&date=${date}`;
-      console.log(date, 'key', monthlyVoteInfo.data, idx);
-
       if (monthlyVoteInfo.data[0])
         return `/vote/library/scroll/month?flag=${
           monthlyVoteInfo.data[monthlyVoteInfo.data.length - 1].id
         }&date=${date}`;
-      return;
+      return null;
     },
     picmeGetFetcher,
     {
