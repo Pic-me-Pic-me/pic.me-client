52b33ab00cca0e081b2c799dc905298398c727aa
pinktopaz <heather128@ewhain.net>
2023-02-09 22:02:07 +0900

    feat : 세로 무한스크롤 서버 구조 변경 적용

--- src/lib/hooks/useGetAllVoteInfo.tsx
+++ src/lib/hooks/useGetAllVoteInfo.tsx
@@ -5,18 +5,14 @@ import { EndedVoteInfo } from '../../types/library';
 import { picmeGetFetcher } from '../axios';
 
 //세로 무한스크롤
-const getKey = (prevLastDate: number, allVoteInfo: AxiosResponse<EndedVoteInfo[]>) => {
-  if (prevLastDate === 0) {
-    return `/vote/library/scroll/all?flag=${prevLastDate}`;
-  }
-  if (allVoteInfo) {
-    return `/vote/library/scroll/all?flag=${allVoteInfo.data[allVoteInfo.data.length - 1].date}`;
-  }
-  return;
+const getKey = (prevLastDate: number, allVoteInfo: AxiosResponse<EndedVoteInfo>) => {
+  if (prevLastDate === 0) return `/vote/library/scroll/all?flag=${prevLastDate}`;
+  if (allVoteInfo) return `/vote/library/scroll/all?flag=${allVoteInfo.data.dates[allVoteInfo.data.dates.length - 1]}`;
+  return null;
 };
 
 export const useGetAllVoteInfo = () => {
-  const { data, isLoading, error, size, setSize } = useSWRInfinite<AxiosResponse<EndedVoteInfo[]>>(
+  const { data, isLoading, error, size, setSize } = useSWRInfinite<AxiosResponse<EndedVoteInfo>>(
     getKey,
     picmeGetFetcher,
     {
@@ -24,14 +20,10 @@ export const useGetAllVoteInfo = () => {
     },
   );
 
-  const parseResultList = data?.map((item) => item.data).flat();
-
-  console.log(parseResultList);
+  const parseResultList = data?.map((item) => item.data.dates).flat();
 
   return {
-    allVoteInfoList: {
-      list: parseResultList ? parseResultList : [],
-    },
+    allVoteInfoList: { dateList: parseResultList ? parseResultList : [] },
     isLoading,
     isError: error,
     size,
