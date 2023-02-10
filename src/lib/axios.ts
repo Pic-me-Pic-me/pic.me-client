e823e4e057d102b6e2466ad8d23435545060b51f
seohee0112 <ghdtj7@naver.com>
2023-02-10 14:03:59 +0900

    fix : env로 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,9 +3,8 @@ import { Cookies } from 'react-cookie';
 
 const TOKEN = localStorage.getItem('accessToken');
 const cookies = new Cookies();
-
 const client = axios.create({
-  baseURL: 'https://with-picme-api.com',
+  baseURL: process.env.REACT_APP_IP,
 
   headers: {
     'Content-type': 'application/json',
