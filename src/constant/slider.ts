438c1ac48441df756a10958a1566df05a63340eb
seohee0112 <ghdtj7@naver.com>
2023-02-10 23:08:14 +0900

    feat : 슬라이더 관련 상수 추가

--- src/constant/slider.ts
+++ src/constant/slider.ts
@@ -5,3 +5,5 @@ export const IMAGE_MARGIN_RATIO = 0.1;
 export const MOVE_THREE_MARGIN = 3;
 export const MOVE_THREE_SLIDER = 3.5;
 export const SLIDER_ITEM_HALF_WIDTH = 156;
+export const REASON_MENU_ITEM_MAX_INDEX = 3;
+export const REASON_MENU_ITEM_WIDTH = 170;
