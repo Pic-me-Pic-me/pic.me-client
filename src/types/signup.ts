363cf67286571a631e5e4acd3637a5086a114a4f
pinktopaz <heather128@ewhain.net>
2023-02-12 21:30:56 +0900

    chore : 회원가입 관련 인터페이스 추가

--- src/types/signup.ts
+++ src/types/signup.ts
@@ -1,12 +1,46 @@
 export interface SignUpInfo {
-  email: string;
-  password: string;
-  passwordConfirm: string;
+  emailInfo: EmailInfo;
+  passwordInfo: PasswordInfo;
+  passwordConfirmInfo: PasswordComfirmInfo;
 }
 
-export interface NicknameInfo {
-  nickname: string;
+export interface EmailInfo {
+  email: string | null;
+  isValid: boolean;
+  errorMsg: string | null;
 }
+export interface PasswordInfo {
+  password: string | null;
+  isValid: boolean;
+  errorMsg: string | null;
+}
+
+export interface PasswordComfirmInfo {
+  isValid: boolean;
+  errorMsg: string | null;
+}
+
+export interface DefaultNicknameInfo {
+  typedNickname: string;
+  state: 'default';
+  finalNickname: null;
+  errorMsg: null;
+}
+
+export interface ErrorNicknameInfo {
+  typedNickname: string;
+  state: 'error';
+  finalNickname: null;
+  errorMsg: string | null;
+}
+export interface PassNicknameInfo {
+  typedNickname: string;
+  state: 'pass';
+  finalNickname: string;
+  errorMsg: string;
+}
+
+export type NicknameInfo = DefaultNicknameInfo | ErrorNicknameInfo | PassNicknameInfo;
 
 export interface SignUpPostInfo {
   dataInfo: AddAccountInfo;
