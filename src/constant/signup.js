186d05142bdb9de7368d07255660c55467cec068
pinktopaz <heather128@ewhain.net>
2023-02-12 21:39:24 +0900

    chore : 비밀번호 정규식 변경

--- src/constant/signup.js
+++ src/constant/signup.js
@@ -1,5 +1,5 @@
 export const EMAIL_REGEX = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-export const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/;
 export const NICKNAME_MAX_LENGTH = 8;
 export const POSSIBLE_NICKNAME_MSG = '사용 가능한 닉네임입니다.';
 export const IMPOSSIBLE_NICKNAME_MSG = '이미 사용 중인 닉네임입니다.';
