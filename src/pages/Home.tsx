864d1bb750dfe167cbd8ac20ffa92435b835bc34
seohee0112 <ghdtj7@naver.com>
2023-03-04 19:13:26 +0900

    docs : home 테스트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,14 @@ import styled from 'styled-components';
 import { IcPlus } from '../asset/icon';
 import { Header, Nav, VoteList } from '../components/Home';
 import Footer from '../components/Home/Footer';
+import { getAccessToken } from '../lib/token';
 import { votingImageState } from '../recoil/maker/atom';
 
 const Home = () => {
   const navigate = useNavigate();
   const setVotingForm = useSetRecoilState(votingImageState);
 
+  console.log('home', getAccessToken('accessToken'));
   useEffect(() => {
     setVotingForm({ title: '', firstImageUrl: '', secondImageUrl: '' });
   }, []);
