20c951134968651b68117763984fe3f4181cb7e9
seohee0112 <ghdtj7@naver.com>
2023-03-06 17:05:04 +0900

    docs : console.log 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,6 @@ const Home = () => {
   const navigate = useNavigate();
   const setVotingForm = useSetRecoilState(votingImageState);
 
-  console.log('home', getAccessToken('accessToken'));
   useEffect(() => {
     setVotingForm({ title: '', firstImageUrl: '', secondImageUrl: '' });
   }, []);
