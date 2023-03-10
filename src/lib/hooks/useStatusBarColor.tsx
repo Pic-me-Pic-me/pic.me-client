3360f9c9039c73c25f8d4977979681d6c9b482d5
parkhyeonji <iamphj3@naver.com>
2023-03-10 13:02:07 +0900

    feat : useStatusBarColor 커스텀 훅 생성

---
+++ src/lib/hooks/useStatusBarColor.tsx
@@ -0,0 +1,31 @@
+import { useLayoutEffect, useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export const STATUS_BAR_COLOR = {
+  LANDING_PAGE: '#FF736E',
+  AUTH_PAGE: '#1E1F21',
+  GENERAL_PAGE: '#FFFFFF',
+};
+
+export const STATUS_BAR_COLOR_USAGE = new Map([
+  ['/', STATUS_BAR_COLOR.LANDING_PAGE],
+  ['/login', STATUS_BAR_COLOR.AUTH_PAGE],
+  ['/mypage', STATUS_BAR_COLOR.AUTH_PAGE],
+]);
+
+const useStatusBarColor = () => {
+  const location = useLocation();
+
+  const statusBarColor = useMemo(
+    () => STATUS_BAR_COLOR_USAGE.get(location.pathname) ?? STATUS_BAR_COLOR.GENERAL_PAGE,
+    [location],
+  );
+
+  useLayoutEffect(() => {
+    const metaElement = document.getElementById('status-bar');
+    if (!metaElement) return;
+    (metaElement as HTMLMetaElement).content = statusBarColor;
+  }, [statusBarColor]);
+};
+
+export default useStatusBarColor;
