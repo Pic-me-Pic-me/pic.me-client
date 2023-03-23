992dda667d2159ad38c2e23fbb5d053ed86e8528
seohee0112 <ghdtj7@naver.com>
2023-03-23 09:49:44 +0900

    feat : 안내 문구 관련 상수 선언

---
+++ src/constant/playerInfo.ts
@@ -0,0 +1,4 @@
+export const VOTING_TITLE = '사진을 선택한 이유를 골라주세요';
+export const FLOWER_VOTING_TITLE = '사진과 어울리는 꽃을 선택해주세요!';
+export const SELECT_REASON = '이유 확정하기';
+export const CONFIRM_REASON = '확인';
