f6c163b6a9b8533847a92d2c82be5512cd197f17
seohee0112 <ghdtj7@naver.com>
2023-03-30 20:41:20 +0900

    fix : 꽃 관련 정보 키워드 리스트 추가

--- src/constant/FlowerIconList.js
+++ src/constant/FlowerIconList.js
@@ -5,6 +5,7 @@ export const FLOWER_ICON_LIST = [
     id: CHEERYBLOSSOMS,
     name: '벚꽃 Keyword',
     color: '#FF5D5D',
+    keywordList: ['청초함', '우아함', '순수함', '투명함', '단정함'],
     icon: (size) => (
       <svg width={size} height={size} viewBox="0 0 42 41" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clipPath="url(#clip0_3702_480267)">
@@ -291,6 +292,7 @@ export const FLOWER_ICON_LIST = [
     id: LILY,
     name: '백합 Keyword',
     color: '#3973CB',
+    keywordList: ['몽글함', '달달함', '아련함', '화창함', '산뜻함'],
     icon: (size) => (
       <svg width={size} height={size} viewBox="0 0 42 41" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clipPath="url(#clip0_3702_480190)">
@@ -588,6 +590,7 @@ export const FLOWER_ICON_LIST = [
     id: SUNFLOWER,
     name: '해바라기 Keyword',
     color: '#3973CB',
+    keywordList: ['열정적', '활달함', '따수움', '해맑음', '긍정적'],
     icon: (size) => (
       <svg width={size} height={size} viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
@@ -725,6 +728,8 @@ export const FLOWER_ICON_LIST = [
     id: ROSE,
     name: '장미 Keyword',
     color: '#FF5D5D',
+
+    keywordList: ['고귀함', '치명적', '강렬함', '매혹적', '화려함'],
     icon: (size) => (
       <svg width={size} height={size} viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
