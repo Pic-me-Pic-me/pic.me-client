06659d6d8379794954d148e649d81e9738d39202
parkhyeonji <iamphj3@naver.com>
2023-03-30 21:27:58 +0900

    feat : 배너 버튼 구현

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -2,10 +2,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Slider from 'react-slick';
 import styled from 'styled-components';
 
-import { IcMainBanner, IcNextArrow, IcSubBanner } from '../../asset/icon';
+import { IcFlowerVote, IcMainBanner, IcNextArrow, IcNormalVote, IcSubBanner } from '../../asset/icon';
 import useModal from '../../lib/hooks/useModal';
 import Guide from './Guide';
 
@@ -20,26 +21,64 @@ const SLIDER_SETTINGS = {
   cssEase: 'linear',
 };
 
-const SLIDER_IMAGES = [<IcMainBanner key="0" />, <IcSubBanner key="1" />];
+const SLIDER_IMAGES = [
+  {
+    id: 0,
+    bannerImg: <IcMainBanner />,
+    title: '친구들이 보는 ‘나를 닮은 꽃’은?',
+    subTitle: 'EVENT',
+    buttonImg: <IcFlowerVote />,
+    buttonName: '투표 만들기',
+  },
+  {
+    id: 1,
+    bannerImg: <IcSubBanner />,
+    title: '픽미 사용방법 A부터 Z까지',
+    subTitle: '공지',
+    buttonImg: <IcNormalVote />,
+    buttonName: '알아보기',
+  },
+];
 
 const Header = () => {
   const { isShowing, toggle } = useModal();
+  const navigate = useNavigate();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>, index: number) => {
+    if (index === 0) {
+      navigate('/voting/flower');
+    }
+    if (index === 1) {
+      toggle();
+    }
+  };
 
   return (
     <>
       <StBannerWrapper>
         <Slider {...SLIDER_SETTINGS}>
-          {SLIDER_IMAGES.map((item, index) => (
-            <StBannerImg key={index}>{item}</StBannerImg>
+          {SLIDER_IMAGES.map(({ id, bannerImg, subTitle, title, buttonImg, buttonName }) => (
+            <StBannerImg key={id}>
+              {bannerImg}
+              <StBannerBtn type="button" onClick={(e) => handleClick(e, id)}>
+                {buttonImg}
+                <div>
+                  <p>{subTitle}</p>
+                  <h1>{title}</h1>
+                </div>
+                {/* <button type="button" onClick={(e) => handleClick(e, id)}> */}
+                {buttonName}
+                {/* </button> */}
+                <IcNextArrow />
+              </StBannerBtn>
+            </StBannerImg>
           ))}
         </Slider>
-        <StGuideBtn type="button" onClick={toggle}>
+        {/* <StGuideBtn type="button" onClick={toggle}>
           <p>픽미 사용방법 A부터 Z까지</p>
-          <IcNextArrow />
-        </StGuideBtn>
+        </StGuideBtn> */}
         <Guide isShowing={isShowing} handleHide={toggle} />
       </StBannerWrapper>
-      <StMeatBall></StMeatBall>
     </>
   );
 };
@@ -52,11 +91,32 @@ const StBannerWrapper = styled.header`
 
   width: 100%;
   margin-top: 6.5rem;
+  margin-bottom: 4.6rem;
 
   border-radius: 1.2rem;
+
+  & > .slick-slider > .slick-dots > li {
+    width: 0.7rem;
+    height: 0.7rem;
+    background-color: #d9d9d9;
+    border-radius: 0.5rem;
+
+    & > button {
+      display: none;
+    }
+  }
+
+  & > .slick-slider > .slick-dots > .slick-active {
+    width: 0.7rem;
+    height: 0.7rem;
+    background-color: #ff5d67;
+    border-radius: 0.5rem;
+  }
 `;
 
 const StBannerImg = styled.div`
+  position: relative;
+
   & > svg {
     width: 100%;
     height: 100%;
@@ -65,26 +125,37 @@ const StBannerImg = styled.div`
   }
 `;
 
-const StGuideBtn = styled.button`
+const StBannerBtn = styled.button`
   display: flex;
+  position: absolute;
   justify-content: space-between;
   align-items: center;
+  bottom: 0;
 
-  height: 7.4rem;
-  margin-top: -7.5rem;
-  padding: 2.8rem 2.9rem 2.7rem 2.4rem;
+  padding-left: 1rem;
+  padding-right: 2.5rem;
+  width: 100%;
+  height: 6.7rem;
 
   border: none;
-  border-radius: 1.2rem;
-  background-color: rgba(0, 0, 0, 0);
+  background-color: transparent;
+  color: ${({ theme }) => theme.colors.Pic_Color_Gray_6};
+  ${({ theme }) => theme.fonts.Pic_Noto_M_Subtitle_4};
 
-  > p {
-    text-align: center;
-    color: ${({ theme }) => theme.colors.Pic_Color_White};
-    ${({ theme }) => theme.fonts.Pic_Body1_Pretendard_Medium_16}
+  & > div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-left: 1rem;
+    padding-right: 3.6rem;
+
+    & > p {
+      color: ${({ theme }) => theme.colors.Pic_Color_Gray_4};
+      ${({ theme }) => theme.fonts.Pic_Noto_SB_Subtitle_2};
+    }
+    & > h1 {
+      color: ${({ theme }) => theme.colors.Pic_Color_Gray_6};
+      ${({ theme }) => theme.fonts.Pic_Noto_SB_Subtitle_2};
+    }
   }
-`;
-
-const StMeatBall = styled.div`
-  height: 4.6rem;
 `;
