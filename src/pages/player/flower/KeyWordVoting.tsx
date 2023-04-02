8583d5c83ba70c3148b332e7b6f20a64c6de246e
seohee0112 <ghdtj7@naver.com>
2023-04-02 19:34:34 +0900

    fix : 첫번째 태그 선택후 버튼 이벤트 사용 오류 해결

--- src/pages/player/flower/KeyWordVoting.tsx
+++ src/pages/player/flower/KeyWordVoting.tsx
@@ -15,14 +15,14 @@ const KeyWordVoting = () => {
   const { keywordIdx } = playerStickerInfo;
 
   const handleVotingSuccess = () => {
-    keywordIdx && navigate('/player/sticker_voting');
+    keywordIdx !== undefined && navigate('/player/sticker_voting');
   };
 
   const handlePrevpage = () => {
     navigate(-1);
   };
   const handleTagOverClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!(e.target instanceof HTMLLIElement) && !(e.target instanceof HTMLButtonElement) && keywordIdx === undefined)
+    if (!(e.target instanceof HTMLLIElement) || (e.target instanceof HTMLButtonElement && keywordIdx === undefined))
       setPlayerStickerInfo({ ...playerStickerInfo, keywordIdx: undefined });
   };
 
