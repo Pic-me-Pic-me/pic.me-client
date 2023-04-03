8da1a250e34ae29bdfae5cecfcf235ff0cc8caa2
seohee0112 <ghdtj7@naver.com>
2023-04-03 14:04:02 +0900

    fix : 프리티어 에러 해결

--- src/pages/player/flower/KeyWordVoting.tsx
+++ src/pages/player/flower/KeyWordVoting.tsx
@@ -16,17 +16,14 @@ const KeyWordVoting = () => {
 
   const handleVotingSuccess = () => {
     keywordIdx !== undefined && navigate('/player/sticker_voting');
-
   };
 
   const handlePrevpage = () => {
     navigate(-1);
   };
   const handleTagOverClick = (e: React.MouseEvent<HTMLDivElement>) => {
-
     if ((!(e.target instanceof HTMLLIElement) || e.target instanceof HTMLButtonElement) && keywordIdx === undefined)
       setPlayerStickerInfo({ ...playerStickerInfo, keywordIdx: undefined });
-    }
   };
 
   useEffect(() => {
