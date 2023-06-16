93d308821dd9a3823ac699dff58e8b948d647a3a
seohee0112 <ghdtj7@naver.com>
2023-06-16 16:32:20 +0900

    docs : 주석 삭제 후 파일 정리

--- src/utils/setNotification.ts
+++ src/utils/setNotification.ts
@@ -1,8 +1,3 @@
-declare global {
-  interface Window {
-    safari?: any;
-  }
-}
 export const regisertWorker = async () => {
   // (B1) 공유키
   const publicKey = 'YOUR-PUBLIC-KEY';
@@ -10,13 +5,7 @@ export const regisertWorker = async () => {
   const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
   // (B2) 서비스 워커 등록 및 확인
   const registration = await navigator.serviceWorker.register(swUrl);
-  // .then((registration) => {
-  //   console.log('Service worker registration succeeded:', registration);
-  // })
-  // .catch((err) => {
-  //   console.log('Service worker registration failed:', err);
-  // });
-  console.log(registration);
+
   alert(registration.active);
   if (registration && registration.active) {
     // (B3)서버에 구독하기
@@ -36,26 +25,4 @@ export const regisertWorker = async () => {
       .then((txt) => console.log(txt))
       .catch((err) => console.error(err));
   }
-  //   }
-  // reg.pushManager
-  //   .subscribe({
-  //     userVisibleOnly: true,
-  //     applicationServerKey: publicKey,
-  //   })
-  //   .then(
-  //     // (B3-1) OK - TEST PUSH NOTIFICATION
-  //     (sub) => {
-  //       fetch('/mypush', {
-  //         method: 'POST',
-  //         body: JSON.stringify(sub),
-  //         headers: { 'content-type': 'application/json' },
-  //       })
-  //         .then((res) => res.text())
-  //         .then((txt) => console.log(txt))
-  //         .catch((err) => console.error(err));
-  //     },
-
-  //     // (B3-2) ERROR!
-  //     (err) => console.error(err),
-  //   );
 };
