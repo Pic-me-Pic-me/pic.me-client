5f6dbcd42395f5d851133594baae6f766b8b887e
seohee0112 <ghdtj7@naver.com>
2023-06-16 15:56:29 +0900

    refactor : async await로 리팩토링

--- src/utils/setNotification.ts
+++ src/utils/setNotification.ts
@@ -4,45 +4,57 @@ declare global {
   }
 }
 export const regisertWorker = async () => {
-  // (B1) YOUR PUBLIC KEY - CHANGE TO YOUR OWN!
+  // (B1) 공유키
   const publicKey = 'YOUR-PUBLIC-KEY';
 
   const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-  // (B2) REGISTER SERVICE WORKER
-  navigator.serviceWorker
-    .register(swUrl)
-    .then((registration) => {
-      console.log('Service worker registration succeeded:', registration);
-    })
-    .catch((err) => {
-      console.log('Service worker registration failed:', err);
+  // (B2) 서비스 워커 등록 및 확인
+  const registration = await navigator.serviceWorker.register(swUrl);
+  // .then((registration) => {
+  //   console.log('Service worker registration succeeded:', registration);
+  // })
+  // .catch((err) => {
+  //   console.log('Service worker registration failed:', err);
+  // });
+  console.log(registration);
+  if (registration) {
+    // (B3)서버에 구독하기
+    const subscription = await registration.pushManager.subscribe({
+      applicationServerKey: publicKey,
+      userVisibleOnly: true,
     });
-  // (B3) SUBSCRIBE TO PUSH SERVER
-  navigator.serviceWorker.ready.then((reg) => {
-    console.log(reg, reg.pushManager);
-    console.log(window.safari, window);
-    // if (window.safari) {
-    // }
-    // reg.pushManager
-    //   .subscribe({
-    //     userVisibleOnly: true,
-    //     applicationServerKey: publicKey,
-    //   })
-    //   .then(
-    //     // (B3-1) OK - TEST PUSH NOTIFICATION
-    //     (sub) => {
-    //       fetch('/mypush', {
-    //         method: 'POST',
-    //         body: JSON.stringify(sub),
-    //         headers: { 'content-type': 'application/json' },
-    //       })
-    //         .then((res) => res.text())
-    //         .then((txt) => console.log(txt))
-    //         .catch((err) => console.error(err));
-    // },
 
-    // (B3-2) ERROR!
-    // (err) => console.error(err),
-    //   );
-  });
+    await fetch('/save-subscription', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(subscription),
+    })
+      .then((res) => res.text())
+      .then((txt) => console.log(txt))
+      .catch((err) => console.error(err));
+  }
+  //   }
+  // reg.pushManager
+  //   .subscribe({
+  //     userVisibleOnly: true,
+  //     applicationServerKey: publicKey,
+  //   })
+  //   .then(
+  //     // (B3-1) OK - TEST PUSH NOTIFICATION
+  //     (sub) => {
+  //       fetch('/mypush', {
+  //         method: 'POST',
+  //         body: JSON.stringify(sub),
+  //         headers: { 'content-type': 'application/json' },
+  //       })
+  //         .then((res) => res.text())
+  //         .then((txt) => console.log(txt))
+  //         .catch((err) => console.error(err));
+  //     },
+
+  //     // (B3-2) ERROR!
+  //     (err) => console.error(err),
+  //   );
 };
