c5638581b30a3af9542f4345d34ac83f5a4c9d85
Brokyeom <goindol40@gmail.com>
2023-06-19 18:27:52 +0900

    chore : 불필요한 코드 삭제

--- src/components/CurrentVote/OriginVoteStatus.tsx
+++ src/components/CurrentVote/OriginVoteStatus.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 
@@ -13,8 +13,6 @@ import { pictureCurrentIdx, pictureResultState, stickerResultState, voteResultSt
 const CurrentVoteDetail = () => {
   const { voteid: voteId } = useParams<{ voteid: string }>();
   const navigate = useNavigate();
-  const location = useLocation();
-  const isFlower = location.state;
 
   const { voteResult, isLoading, isError } = useGetCurrentVote(voteId);
 
@@ -36,7 +34,6 @@ const CurrentVoteDetail = () => {
     if (voteResult) {
       setVoteResult(voteResult);
       setPictureResultState(voteResult.Picture);
-      console.log(isFlower);
     }
   }, [currentIdx, voteResult]);
 
