98577cb033129eaabe96d2fa6253d93f0b2ca7d8
seohee0112 <ghdtj7@naver.com>
2023-06-21 16:23:27 +0900

    docs : console.log 추가

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -52,7 +52,7 @@ self.addEventListener('activate', (event) => {
       ),
     ),
   );
-  registerWorker();
+  console.log(navigator.serviceWorker);
   consoleMessage('ACTIVE');
 });
 self.addEventListener('push', (event: PushEvent) => {
@@ -66,26 +66,3 @@ self.addEventListener('push', (event: PushEvent) => {
     }),
   );
 });
-const registerWorker = async () => {
-  // (B1) 공유키
-  const publicKey = process.env.REACT_APP_PUBLIC_SUBSCRIBE_KEY;
-
-  const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-  // (B2) 서비스 워커 등록 및 확인
-  const registration = await navigator.serviceWorker.register(swUrl);
-
-  if (registration.active) {
-    // (B3)서버에 구독하기
-    const subscription = await registration.pushManager.subscribe({
-      applicationServerKey: publicKey,
-      userVisibleOnly: true,
-    });
-    console.log(JSON.stringify(subscription));
-
-    await client
-      .post('alarm/register', subscription)
-      .then((res) => res.data)
-      .then((txt) => console.log(txt))
-      .catch((err) => console.error(err));
-  }
-};
