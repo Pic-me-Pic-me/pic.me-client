a790e752a699c1cda7e89991dd18f82aa7de045f
Brokyeom <goindol40@gmail.com>
2023-06-23 15:17:25 +0900

    feat : webpack 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
 module.exports = {
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   mode: 'production',
   webpack(config) {
     config.module.rules.push({
